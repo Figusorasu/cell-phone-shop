a5553448a455cd8ba6b6837a57137039117d8352
Figus <99464311+Figusorasu@users.noreply.github.com>
2023-02-06 12:49:27 +0100

    proj start

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "phone.hpp"
+
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
---
+++ phone.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+class Phone {
+
+	string brandName; // ex. "Apple"
+	string modelName; // ex. "iPhone 13"
+	int formFactor; // 1 - Bar, 2 - Smartphone, 3 - Tablet, 4 - Stationary
+	char[4] yearOfIssue; // ex. "2020"
+	float screenSize; // ex. 7.3f (7,3 inches)
+
+
+
+};
